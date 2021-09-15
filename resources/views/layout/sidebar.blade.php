86e6cb45abb3ffb6903af2a57e8a870cc042fead
Aniefon Umanah <aniefon.umanah@gmail.com>
2021-09-15 22:38:19 +0100

    Added initial page layout

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar-wrapper">
+    <ul class="nav">
+        <li>
+            <a href="./map.html">
+                <i class="nc-icon nc-pin-3"></i>
+                <p>Admins</p>
+            </a>
+        </li>
+        <li class="active ">
+            <a href="./dashboard.html">
+                <i class="nc-icon nc-bank"></i>
+                <p>Companies</p>
+            </a>
+        </li>
+        <li>
+            <a href="./icons.html">
+                <i class="nc-icon nc-diamond"></i>
+                <p>Employees</p>
+            </a>
+        </li>
+
+    </ul>
+</div>
